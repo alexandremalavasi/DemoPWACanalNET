80d82b6da991ca7fe34244db86fa74dda04266cd
Alexandre Felipe Malavasi Cardoso <alexandremalavasi@hotmail.com>
2018-07-16 18:16:47 -0300

    first commit

--- DemoPWACanalNET/Controllers/HomeController.cs
+++ DemoPWACanalNET/Controllers/HomeController.cs
@@ -15,23 +15,27 @@ namespace DemoPWACanalNET.Controllers
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult News()
         {
-            ViewData["Message"] = "Your application description page.";
+            List<News> list = new List<News>();
 
+            for(int i = 1; i < 50; i++)
+            {
+                News news = new News();
+                news.Title = "Título " + i.ToString();
+                news.Description = "Os franceses voltam ao topo do mundo. Em um decisão com gol contra e uso do VAR inéditos em finais de Copa do Mundo, além de invasão de gramado por um grupo radical russo, a França derrotou a Croácia por 4 a 2, neste domingo, no Estádio Olímpico Lujniki, em Moscou, e conquistou o título mundial de futebol pela segunda vez na história. Mandzukic (contra), Griezmann, Pogba e Mbappé fizeram os gols da equipe azul, enquanto Perisic e Madzukic, este se aproveitando de uma falha bizarra do goleiro e capitão Lloris, descontaram para a Croácia. ";
+                news.Photo = "../images/copa-do-mundo.jpg";
+
+                list.Add(news);
+            }
+
+            return View(list);
+        }
+
+        public IActionResult Cadastro()
+        {
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
--- DemoPWACanalNET/Startup.cs
+++ DemoPWACanalNET/Startup.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebEssentials.AspNetCore.Pwa;
 
 namespace DemoPWACanalNET
 {
@@ -21,7 +23,15 @@ namespace DemoPWACanalNET
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddProgressiveWebApp(new PwaOptions
+            {
+                //CacheId = "Worker " + version,
+                Strategy = ServiceWorkerStrategy.CacheFirst,
+                RoutesToPreCache = "/Home/News"
+            });
+
             services.AddMvc();
+            services.AddSingleton(typeof(IHttpContextAccessor), typeof(HttpContextAccessor));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
